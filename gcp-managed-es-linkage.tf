062c75c2b8391dd3696ee429c55e8623417f014e
Marty Henderson <henderson.marty@gmail.com>
2022-12-19 14:34:54 +0000

    Updating to configure the psc connection

--- gcp-managed-es-linkage.tf
+++ gcp-managed-es-linkage.tf
@@ -1,18 +1,4 @@
-resource "google_dns_managed_zone" "managed_es_psc_dns" {
-  dns_name      = "psc.us-central1.gcp.cloud.es.io."
-  force_destroy = false
-  name          = "managed-es-psc-dns"
-  project       = var.google_project
-  visibility    = "private"
-}
-
-resource "google_service_directory_namespace" "es_psc_default" {
-  provider     = google-beta
-  location     = var.google_region
-  namespace_id = "es-psc-default"
-  project      = var.google_project
-}
-
+# Create the VPC for the connection to the managed elasticsearch instance.
 resource "google_compute_network" "managed_elastic_psc_vpc" {
   auto_create_subnetworks = false
   mtu                     = 1460
@@ -20,7 +6,6 @@ resource "google_compute_network" "managed_elastic_psc_vpc" {
   project                 = var.google_project
   routing_mode            = "REGIONAL"
 }
-
 resource "google_compute_subnetwork" "psc_sub_central1" {
   ip_cidr_range              = "10.0.0.0/24"
   name                       = "psc-sub-central1"
@@ -33,6 +18,15 @@ resource "google_compute_subnetwork" "psc_sub_central1" {
   stack_type                 = "IPV4_ONLY"
 }
 
+# Create the namespace for the private service connection.
+resource "google_service_directory_namespace" "es_psc_default" {
+  provider     = google-beta
+  location     = var.google_region
+  namespace_id = "es-psc-default"
+  project      = var.google_project
+}
+
+# Set the ip address for the connection.
 resource "google_compute_address" "managed_elastic_cloud_ip" {
   address      = "10.0.0.2"
   address_type = "INTERNAL"
@@ -43,11 +37,15 @@ resource "google_compute_address" "managed_elastic_cloud_ip" {
   subnetwork   = google_compute_subnetwork.psc_sub_central1.id
 }
 
+# Create the actual private service connection.
 resource "google_compute_forwarding_rule" "managed_es_psc_test" {
-  name         = "managed-es-psc-test"
-  network      = google_compute_network.managed_elastic_psc_vpc.id
-  project      = var.google_project
-  region       = var.google_region
+  provider              = google-beta
+  name                  = "managed-es-psc-test"
+  load_balancing_scheme = ""
+  network               = google_compute_network.managed_elastic_psc_vpc.id
+  project               = var.google_project
+  region                = var.google_region
+  ip_address            = google_compute_address.managed_elastic_cloud_ip.id
 
   service_directory_registrations {
     namespace = google_service_directory_namespace.es_psc_default.namespace_id
@@ -55,3 +53,34 @@ resource "google_compute_forwarding_rule" "managed_es_psc_test" {
 
   target = "https://www.googleapis.com/compute/beta/projects/cloud-production-168820/regions/us-central1/serviceAttachments/proxy-psc-production-us-central1-v1-attachment"
 }
+
+# Create the DNS zonbe for the private service connection.
+resource "google_dns_managed_zone" "managed_es_psc_dns" {
+  dns_name      = "psc.us-central1.gcp.cloud.es.io."
+  force_destroy = false
+  name          = "managed-es-psc-dns"
+  project       = var.google_project
+  visibility    = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = google_compute_network.managed_elastic_psc_vpc.id
+    }
+  }
+}
+
+# Assign the ip address reserved to an A record that points at managed elasticsearch.
+resource "google_dns_record_set" "a_record_managed_es" {
+  name         = "*.psc.us-central1.gcp.cloud.es.io."
+  managed_zone = google_dns_managed_zone.managed_es_psc_dns.name
+  type         = "A"
+  ttl          = 300
+  project      = var.google_project
+
+  rrdatas = ["10.0.0.2"]
+}
+
+
+
+# still todo:
+# - once the psc is created we need to apply this as a traffic filter in the managed ES.data
